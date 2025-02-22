041ec09b9b6acb426422e9d02aed7b9fd2d71798
ArtemNikit1n <st091174@student.spbu.ru>
2025-02-22 18:09:45 +0600

    feat: Оптимизировать прямое преобразование и доделать обратное

---
+++ Homeworks/01-BWT/BurrowsWheelerTransformer/BurrowsWheelerTransformer/Program.cs
@@ -0,0 +1,14 @@
+﻿// <copyright file="Program.cs" company="PlaceholderCompany">
+// Copyright (c) PlaceholderCompany. All rights reserved.
+// </copyright>
+
+using BurrowsWheelerTransformer;
+
+const string input = "ba$$nana$";
+
+var (transformedString, endPosition) = BWT.Transform(input);
+Console.WriteLine($"Transformed string: {transformedString}");
+Console.WriteLine($"End position: {endPosition}");
+
+var result = BWT.InverseTransform(transformedString, endPosition);
+Console.WriteLine($"The original string: {result}");
