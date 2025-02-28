db58aa2d5b123c2b3085b780653010ee631e49d2
ArtemNikit1n <st091174@student.spbu.ru>
2025-02-28 17:03:29 +0600

    feat: Практика по тестированию

---
+++ PracticalTasks/StackCalculator/StackCalculator.Tests/StackTests.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+
+namespace StackCalculator.Tests;
+
+public class StackTests
+{
+    private static readonly double[] TestData = [0, 1, 2, 3, 1000];
+
+    private static IStack[] Stacks =>
+    [
+        new ArrayStack(),
+        new LinkedStack()
+    ];
+    
+    [Test]
+    public void Stack_Pop_ShouldReturnPushedValue([ValueSource(nameof(Stacks))] IStack stack, [ValueSource(nameof(TestData))] double value)
+    {
+        stack.Push(value);
+        Assert.That(stack.Pop(), Is.EqualTo((value, false)));
+    }
+
+    [Test]
+    public void Stack_Pop_ShouldReturnPushedValues_Multiple([ValueSource(nameof(Stacks))] IStack stack)
+    {
+        foreach (var t in TestData)
+        {
+            stack.Push(t);
+        }
+
+        for (var i = 0; i < TestData.Length; i++)
+        {
+            Assert.That(stack.Pop(), Is.EqualTo((TestData[^(i + 1)], false)));
+        }
+    }
+
+    [Test]
+    public void Stack_IsEmpty_ForEmptyStack_ShouldReturnTrue([ValueSource(nameof(Stacks))] IStack stack) 
+        => Assert.That(stack.IsEmpty, Is.True);
+
+    [Test]
+    public void Stack_IsEmpty_ForNonEmptyStack_ShouldReturnFalse([ValueSource(nameof(Stacks))] IStack stack)
+    {
+        stack.Push(1);
+        Assert.That(stack.IsEmpty, Is.False);
+    }
+}
--- PracticalTasks/StackCalculator/StackCalculator/IStack.cs
+++ PracticalTasks/StackCalculator/StackCalculator/IStack.cs
@@ -1,3 +1,7 @@
+// <copyright file="IStack.cs" company="PlaceholderCompany">
+// Copyright (c) PlaceholderCompany. All rights reserved.
+// </copyright>
+
 namespace StackCalculator;
 
 /// <summary>
