497d1ae714cdd42e04e4cb99d071f351c5807d1f
ArtemNikit1n <st091174@student.spbu.ru>
2025-03-08 14:07:25 +0600

    chore: create project

---
+++ Homeworks/03-LZW/LZW/LZW.cs
@@ -0,0 +1,14 @@
+namespace LZW;
+
+public static class LZW
+{
+    public static void Compress(string input)
+    {
+        Console.WriteLine(input);
+    }
+
+    public static void Decompress(string compressedString)
+    {
+        Console.WriteLine(compressedString);
+    }
+}
