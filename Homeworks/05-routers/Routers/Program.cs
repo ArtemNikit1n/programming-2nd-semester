22966a961775dd43261a657104e701404a3837dd
ArtemNikit1n <artemnikitin24453@gmail.com>
2025-03-28 22:45:20 +0300

    feat: Добавить пользовательский интерфейс

--- Homeworks/05-routers/Routers/Program.cs
+++ Homeworks/05-routers/Routers/Program.cs
@@ -2,4 +2,42 @@
 // Copyright (c) PlaceholderCompany. All rights reserved.
 // </copyright>
 
-Console.WriteLine("Hello, World!");
+using Routers;
+
+try
+{
+    if (args.Length != 2)
+    {
+        Console.Error.WriteLine("Usage: Routers <input_file> <output_file>");
+        return 1;
+    }
+
+    var inputFile = args[0];
+    var outputFile = args[1];
+
+    var graph = new Graph(inputFile);
+    var mst = graph.GetMaximumSpanningTree();
+    mst.SaveToFile(outputFile);
+
+    return 0;
+}
+catch (FileNotFoundException ex)
+{
+    Console.Error.WriteLine($"Error: {ex.Message}");
+    return 1;
+}
+catch (FormatException ex)
+{
+    Console.Error.WriteLine($"Error: Invalid input format. {ex.Message}");
+    return 1;
+}
+catch (GraphNotConnectedException)
+{
+    Console.Error.WriteLine("Error: The graph is not connected.");
+    return 1;
+}
+catch (Exception ex)
+{
+    Console.Error.WriteLine($"Unexpected error: {ex.Message}");
+    return 1;
+}
