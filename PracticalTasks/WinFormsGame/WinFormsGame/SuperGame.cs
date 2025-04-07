f954f618ecbe07ab6f36f0f6393707bf739afcac
ArtemNikit1n <artemnikitin24453@gmail.com>
2025-04-07 20:57:39 +0300

    chore: Создать проект

---
+++ PracticalTasks/WinFormsGame/WinFormsGame/SuperGame.cs
@@ -0,0 +1,16 @@
+// <copyright file="SuperGame.cs" company="PlaceholderCompany">
+// Copyright (c) PlaceholderCompany. All rights reserved.
+// </copyright>
+
+namespace WinFormsGame;
+
+public partial class SuperGame : Form
+{
+    /// <summary>
+    /// Initializes a new instance of the <see cref="SuperGame"/> class.
+    /// </summary>
+    public SuperGame()
+    {
+        this.InitializeComponent();
+    }
+}
